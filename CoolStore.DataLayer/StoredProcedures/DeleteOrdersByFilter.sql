65151474ead01dd159b11fd108b4d7c2daea93dc
Aliaksandr Beliakou <Aliaksandr_Beliakou@epam.com>
2022-05-27 00:09:34 +0300

    [db] Add Stored procedures for Find and Delete by Filter

---
+++ CoolStore.DataLayer/StoredProcedures/DeleteOrdersByFilter.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[DeleteOrdersByFilter]
+	@ProductId int null,
+	@Year int null,
+	@Month int null,
+	@status nvarchar(50) null
+AS
+BEGIN
+	DELETE FROM Orders WHERE 
+		((YEAR(CreateDate) = @Year or YEAR(UpdateDate) = @Year) or @Year is null)
+		and ((MONTH(CreateDate) = @Month or MONTH(UpdateDate) = @Month) or @Month is null)
+		and (Status = @Status or @Status is null)
+		and (ProductId = @ProductId or @ProductId is null)
+END
+
---
+++ CoolStore.DataLayer/StoredProcedures/GetOrdersByFilter.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetOrdersByFilter]
+	@ProductId int null,
+	@Year int null,
+	@Month int null,
+	@Status nvarchar(50) null
+AS
+BEGIN
+	SELECT * FROM Orders WHERE 
+		((YEAR(CreateDate) = @Year or YEAR(UpdateDate) = @Year) or @Year is null)
+		and ((MONTH(CreateDate) = @Month or MONTH(UpdateDate) = @Month) or @Month is null)
+		and (Status = @Status or @Status is null)
+		and (ProductId = @ProductId or @ProductId is null)
+END
